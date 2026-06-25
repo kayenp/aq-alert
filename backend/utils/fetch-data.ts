0385e93d2b2d752778689852f5ffc73b75ac9ac5
KenP <githubdotcom20250603_1349@merchnt.online>
2026-06-25 12:59:55 -0700

    Initial commit

---
+++ backend/utils/fetch-data.ts
@@ -0,0 +1,6 @@
+import { weatherData } from "./openmeteo.js";
+export { currPm2_5, currPm10 };
+// Open-meteo results
+const currPm2_5 = weatherData.current.pm2_5;
+const currPm10 = weatherData.current.pm10;
+
