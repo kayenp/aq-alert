06fdeb04f819306ffe3fcbf67b3aeed8fdf537ec
Ken <githubdotcom20250603_1349@merchnt.online>
2026-06-25 15:49:17 -0700

    WIP

--- backend/index.ts
+++ backend/index.ts
@@ -1,2 +1,6 @@
+import { weatherData } from './utils/openmeteo.js';
+import { pm2_5_8Hour, pm10_8Hour } from './utils/calculations.js';
 
+console.log(weatherData.hourly.pm2_5)
+console.log(pm2_5_8Hour, pm10_8Hour);
  
