0d9b4e717ed91cf324f6ecfd36bd4a66a8f88cea
Ken <githubdotcom20250603_1349@merchnt.online>
2026-07-18 18:10:57 -0700

    Barebones Node server

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,52 @@
-import { weatherData } from './utils/openmeteo.js';
-import { pm2_5, pm10, truncateData } from './utils/calculations.js';
+// import { weatherData } from './utils/openmeteo.js';
+// import { pm2_5, pm10, truncateData } from './utils/calculations.js';
 
-console.log(weatherData.hourly.pm2_5)
-console.log(pm2_5, pm10);
-console.log(truncateData(pm2_5))
- 
+// console.log(weatherData.hourly.pm2_5)
+// console.log(pm2_5, pm10);
+// console.log(truncateData(pm2_5));
+
+// nodejs imports
+import https from 'https';
+import fs from 'fs/promises';
+import path from 'path';
+import url from 'url';
+
+// express imports
+import express from 'express';
+import cors from 'cors';
+
+// constants
+const PORT = process.env.PORT || 5000;
+const __dirname = import.meta.dirname;
+const __prevDir = path.join(__dirname, '..');
+const __publicDir = path.join(__prevDir, '/', 'public');
+const app = express();
+const server = https.createServer();
+
+// servers
+app.listen(3000, (err) => {
+	if (err) {
+		if (err instanceof Error) {
+			return err;
+		}
+		return Error(err);
+	};
+	console.log("Express running on port 3000");
+});
+
+server.listen(PORT, () => {
+	console.log(`NodeJS running on port ${PORT}`);
+});
+
+
+// middleware
+app.use(cors(), (req, res, next) => next());
+app.use(express.json(), (req, res, next) => next());
+app.use(express.static('public'), (req, res, next) => next());
+
+// GET
+app.get('/', (req, res, next) => {
+	res.sendFile('index.html', err => {
+		err && next(err);
+	});
+});
