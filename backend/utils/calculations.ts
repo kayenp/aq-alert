920fb4f4ef6b27de050610d698f46da803674cbf
Ken <githubdotcom20250603_1349@merchnt.online>
2026-07-18 18:09:48 -0700

    Minor changes

--- backend/utils/calculations.ts
+++ backend/utils/calculations.ts
@@ -6,7 +6,7 @@ const pm2_5 = {
     name: 'pm2.5',
     current: data.current.pm2_5,
     hourly: data.hourly.pm2_5, // array
-    hours_1: data.hourly.pm2_5[0],
+    hours_1: data.hourly.pm2_5?.[0],
     hours_8: data.hourly.pm2_5?.slice(0,8).reduce((a, cV) => a + cV)!/8, 
 }
 
@@ -14,12 +14,12 @@ const pm10 = {
     name: 'pm10',
     current: data.current.pm10,
     hourly: data.hourly.pm10, // array
-    hours_1: data.hourly.pm10[0],
+    hours_1: data.hourly.pm10?.[0],
     hours_8: data.hourly.pm10?.slice(0,8).reduce((a, cV) => a + cV)!/8, 
 }
 
 // Truncate pollutant data
-export function truncateData(pollutant: { field : string | number }) {
+export function truncateData(pollutant: ) {
     switch(pollutant.field) {
         
         // round to 1 decimal
@@ -35,7 +35,7 @@ const pm2_5_break = [[0.0, 9.0], [9.1, 35.4], [35.5, 55.4], [55.5, 125.4], [125.
 const pm10_break = [[0, 54], [55, 154], [155, 254], [255, 354], [355, 424], [425]];
 
 function preciseRound(value: number, decimal: number) {
-    return Number(Math.round((value+'e'+decimal) as any)+'e-'+decimal);
+    return Number(Math.round((value +'e'+ decimal) as any) +'e-'+ decimal);
 };
 
 function calcAqi(pollutant: string, concentration: number) {
